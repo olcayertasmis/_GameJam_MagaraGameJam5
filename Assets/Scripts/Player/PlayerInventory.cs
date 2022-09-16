5cc26a53d21240233d4adc76a168ad37e028101e
LemonAndMint <orn.betul@gmail.com>
2022-09-16 11:56:53 +0300

    Added multiple object pickup system

--- Assets/Scripts/Player/PlayerInventory.cs
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerInventory : MonoBehaviour
 {
     public GameObject objectInHand;
+    public List<GameObject> inventory;
     public PlayerRaycaster raycaster;
 
     public void Pickup(){
@@ -12,16 +13,43 @@ public class PlayerInventory : MonoBehaviour
         GameObject tempObject = raycaster.RaycastFromCamera();
         
         if(tempObject != null){
+
             objectInHand = tempObject;
+            inventory.Add(objectInHand);
+
             _takeObjectToHand();
+            _deactiveInventory();
+        
         }
 
     }
 
-    private void _takeObjectToHand(){ //#TODO bunu ele gore degistirecegiz / Corpyr
+    public void PlaceObject(){
 
+        //#TODO bomba gibi seyleri yerlestirme kismi / Corpyr
+
+    }
+
+    private void _takeObjectToHand(){ //#TODO pozisyon ve parent ele gore degisecek / Corpyr
+
+        objectInHand.tag = "Untagged";
         objectInHand.transform.parent = transform;
         objectInHand.transform.position = transform.position;
 
     }
+
+    private void _deactiveInventory(){
+
+        foreach(GameObject gameObject in inventory){
+
+            if(gameObject.name != objectInHand.name){
+
+                gameObject.SetActive(false);
+            
+            }
+
+        }
+
+    }
+
 }
